37d1334062c1ad860747d1445431978b12ad966b
Roope Virtanen <roopekalle16@gmail.com>
2023-02-26 02:18:27 +0200

    Käyttöjärjestelmästä riippumattomat värit?

--- MegaShakkiBotti/varikoodit.h
+++ MegaShakkiBotti/varikoodit.h
@@ -4,26 +4,25 @@
 
 enum Vari
 {
-    musta = 0,
-    punainen = 1,
-    vihrea = 2,
-    keltainen = 3,
-    sininen = 4,
-    purppura = 5,
-    turkoosi = 6,
-    harmaa = 7,
-    tumman_harmaa = 8,
-    kirkkaan_punainen = 9,
-    kirkkaan_vihrea = 10,
-    kirkkaan_keltainen = 11,
-    kirkkaan_sininen = 12,
-    kirkkaan_purppura = 13,
-    kirkkaan_turkoosi = 14,
-    valkoinen = 15,
+    musta = 16,
+    punainen = 88,
+    vihrea = 28,
+    keltainen = 100,
+    sininen = 18,
+    purppura = 93,
+    turkoosi = 27,
+    harmaa = 250,
+    tumman_harmaa = 244,
+    kirkkaan_punainen = 196,
+    kirkkaan_vihrea = 46,
+    kirkkaan_keltainen = 226,
+    kirkkaan_sininen = 21,
+    kirkkaan_purppura = 201,
+    kirkkaan_turkoosi = 51,
+    valkoinen = 231
 };
 
 std::string tekstivari(Vari vari);
 std::string taustavari(Vari vari);
 
 std::string resetoiVarit();
-
